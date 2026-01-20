8a8f44f74908feb50ce1a0fc1c37ca3f290dc60c
oleg.pronchenko <traeop@gmail.com>
2026-01-20 13:39:33 +0200

    chore(deps): update @devisfuture/electron-modular to ^1.1.2
    Updated the @devisfuture/electron-modular dependency to the latest
    version for improved functionality and performance.

--- src/main/app.ts
+++ src/main/app.ts
@@ -22,8 +22,9 @@ app.disableHardwareAcceleration();
 
 Menu.setApplicationMenu(null);
 
+const source = process.env.CSP_CONNECT_SOURCES;
 initSettings({
-  baseRestApi: process.env.BASE_REST_API ?? "",
+  cspConnectSources: source ? [source] : [],
   localhostPort: process.env.LOCALHOST_ELECTRON_SERVER_PORT ?? "",
   folders: {
     distRenderer: folders.distRenderer,
