680f9f787e94ea69f6226f4e8d7c7e16ce7599ac
oleg.pronchenko <traeop@gmail.com>
2026-01-22 22:51:30 +0200

    refactor(ipc): remove unused session import and simplify options
    Removed the unused `session` import and adjusted the `webPreferences`
    to use the `partition` directly instead of the session object.

--- src/main/auth-social-network/ipc.ts
+++ src/main/auth-social-network/ipc.ts
@@ -1,4 +1,3 @@
-import { session } from "electron";
 import { restApi } from "../config.js";
 import { IpcHandler, type TParamOnInit } from "@devisfuture/electron-modular";
 import { ipcMainOn } from "../$shared/utils.js";
@@ -9,14 +8,13 @@ export class AuthSocialNetworkIpc {
 
   onInit({ getWindow }: TParamOnInit<TWindows["authSocialNetwork"]>): void {
     const authSocialNetworkWindow = getWindow("window:auth-social-network");
-    const authSession = session.fromPartition("persist:auth");
 
     ipcMainOn("authSocialNetwork", async (_, { providers }) => {
       authSocialNetworkWindow.create({
         loadURL: `${restApi.urls.base}${restApi.urls.baseApi}${restApi.urls.auth.base}${restApi.urls.auth[providers]}`,
         options: {
           webPreferences: {
-            session: authSession,
+            partition: "persist:auth",
           },
         },
       });
